2f9b7473ca84d615c0c18afbd9713fbf9d9013ac
Tyler Santerre <tyler.santerre@gmail.com>
2015-06-12 08:47:14 -0400

    update igt settings

--- scripts/source/igt.sh
+++ scripts/source/igt.sh
@@ -2,6 +2,7 @@
 export ENG_LAN=10.90.239.
 export SERIAL_IP=10.90.231.109
 export BOARD=root@$ENG_LAN
+export NMS=10.90.231.20
 
 # altera
 export ALTERAOCLSDKROOT="/opt/altera/hld"
@@ -16,8 +17,17 @@ function ssh-board() {
     ssh ${BOARD}${1}
 }
 
+# scp's the specified file to the 10.90.239.x address where x provided the user
+# ex. scp-board Release/Mobility 26
+function scp-board() {
+    scp $1 ${BOARD}${2}:~
+}
+
 # telnet's to 10.90.231.109 100x address where x is provided by the user
 # ex. telnet-board 10 = telnet 10.90.231.109 10010
 function telnet-board() {
     telnet $SERIAL_IP 100${1}
 }
+
+alias ssh-nms="ssh idirect@${NMS}"
+
